67e6277f7f1ef6463a160e0cda55667dd0ed9ce3
L7L71 <45070538+L7L71@users.noreply.github.com>
2018-11-17 13:53:33 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -58,9 +58,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**RS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلأل
-
-
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -74,9 +72,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**RS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلألRS%^&^$%HYTR^&I&KI&^(&O GFTE$%HY%^HYU&%^Uلألإٌ‘÷&^÷،&^،÷&^×&**&؛(&/&^÷ـلأل
-
-
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
